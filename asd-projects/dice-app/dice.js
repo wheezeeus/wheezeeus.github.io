da8c36f40886956991dadec40718359d523fa7ed
Ethan T Leuangkhamsone <ethaleua14@bossierschools.org>
2026-03-16 13:46:50 -0500

    die app

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,61 @@
 $(document).ready(function () {
   // Your code goes here
+  $("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", 42)
+  .css("left", 42)
+  .appendTo("#die");
 
 });
+
+function makeDot(top, left, elementID) {$("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", top)
+  .css("left", left)
+  .appendTo(elementID);
+
+}
+
+function rollDie(dieID) {
+  $(dieID).empty()
+  var randomNum = Math.ceil(Math.random() * 6);
+  if (randomNum === 1) {
+    makeDot(50, 50, dieID); // middle middle
+  } else if (randomNum === 2) {
+    makeDot(25, 25, dieID); // top left
+    makeDot(75, 75, dieID); // bottom right
+  } else if (randomNum === 3) {
+    makeDot(25, 25, dieID); // top left
+    makeDot(75, 75, dieID); // bottom right
+    makeDot(50, 50, dieID); // middle middle
+  } else if (randomNum === 4) {
+    makeDot(75, 75, dieID); // bottom right
+    makeDot(25, 25, dieID); // top left
+    makeDot(25, 75, dieID); // bottom left
+    makeDot(75, 25, dieID); // top right
+  } else if (randomNum === 5) {
+    makeDot(50, 50, dieID); // middle middle
+    makeDot(75, 75, dieID); // bottom right
+    makeDot(25, 25, dieID); // top left
+    makeDot(25, 75, dieID); // bottom left
+    makeDot(75, 25, dieID); // top right
+  } else if (randomNum === 6) {
+    makeDot(75, 75, dieID); // bottom right
+    makeDot(50, 75, dieID)
+    makeDot(50, 25, dieID)
+    makeDot(25, 25, dieID); // top left
+    makeDot(25, 75, dieID); // bottom left
+    makeDot(75, 25, dieID); // top right
+  }
+}
+function handleClick() {
+  rollDie('#die')
+}
+
+$('#die').on('click', handleClick)
