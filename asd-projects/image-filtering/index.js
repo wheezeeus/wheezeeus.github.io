b7223229a125d333a4e00f7898fb13ecf59c137b
Ethan T Leuangkhamsone <ethaleua14@bossierschools.org>
2026-03-26 12:45:19 -0500

    complete image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,9 +20,10 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
-  
-
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilter(increaseGreenbyBlue);
+  applyFilterNoBackground(reddify);
   // do not change the below line of code
   render($("#display"), image);
 }
@@ -32,18 +33,59 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
-
+function applyFilter(filterFunction) {
+  for (i = 0; i < image.length; i++) {
+    for (j = 0; j < image[i].length; j++) {
+      var pixel = image[i][j]
+      var pixelArray = rgbStringToArray(pixel)
+      // This is where I’ll modify the color values later
+      filterFunction(pixelArray)
+      var updatedPixel = rgbArrayToString(pixelArray)
+      image[i][j] = updatedPixel
+    }
+  }
+}
 
 // TODO 9 Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction) {
+  var backgroundColor = image[0][0]
+  for (i = 0; i < image.length; i++) {
+    for (j = 0; j < image[i].length; j++) {
+      var pixel = image[i][j]
+      if (image[i][j] !== backgroundColor) {
+        var pixelArray = rgbStringToArray(pixel)
+        filterFunction(pixelArray)
+        var updatedPixel = rgbArrayToString(pixelArray)
+        image[i][j] = updatedPixel
+      }
+    }
+  }
+}
 
 // TODO 6: Create the keepInBounds function
-
-
+function keepInBounds(number) {
+  if (number < 0) {
+    return 0
+  }
+  else if (number > 255) {
+    return 255
+  }
+  else {
+    return number
+  }
+  }
 // TODO 4: Create reddify filter function
-
-
+function reddify(list) {
+  list[RED] += 200
+  }
 // TODO 7 & 8: Create more filter functions
-
+function decreaseBlue(pixArr) {
+  pixArr[BLUE] -= 50
+  pixArr[BLUE] = keepInBounds(pixArr[BLUE])
+}
+function increaseGreenbyBlue(pixArr) {
+  pixArr[GREEN] += pixArr[BLUE]
+  pixArr[GREEN] = keepInBounds(pixArr[GREEN])
+  }
 
 // CHALLENGE code goes below here
